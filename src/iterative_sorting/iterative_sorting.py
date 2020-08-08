c753aab0e28e2c0a9f52bd2b54efc3b5beec6f78
anitashar <57122557+anitashar@users.noreply.github.com>
2020-08-07 17:50:39 -0700

    Add files via upload

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,3 +1,4 @@
+
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
@@ -6,20 +7,32 @@ def selection_sort(arr):
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
-
+        for idx in range(cur_index+1, len(arr)):
+            if arr[idx] < arr[smallest_index]:
+                smallest_index = idx
 
         # TO-DO: swap
-        # Your code here
-
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
-
+    # for number in range(len(arr)-1,0,-1):
+    #     for i in range(number):
+    #         if arr[i]>arr[i+1]:
+    #             temp = arr[i]
+    #             arr[i] = arr[i+1]
+    #             arr[i+1] = temp
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        #Last i elements are already in place
+        for j in range(0, n-i-1):
+            #traverese the array from o to n-i-1
+            #swap if the element found is greater than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
 
 '''
